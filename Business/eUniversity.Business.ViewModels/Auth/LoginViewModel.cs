0b1e6756225c26798bcf84aff943e85c71164b94
EAlhovik <e.alhovik@gmail.com>
2014-01-13 02:37:16 +0300

    remove unused attributes

--- Business/eUniversity.Business.ViewModels/Auth/LoginViewModel.cs
+++ Business/eUniversity.Business.ViewModels/Auth/LoginViewModel.cs
@@ -1,19 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace eUniversity.Business.ViewModels.Auth
+﻿namespace eUniversity.Business.ViewModels.Auth
 {
     public class LoginViewModel
     {
-//        [Required]
-//        [Display(Name = "User name")]
         public string UserName { get; set; }
 
-//        [Required]
-//        [DataType(DataType.Password)]
-//        [Display(Name = "Password")]
         public string Password { get; set; }
 
-//        [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
 }
