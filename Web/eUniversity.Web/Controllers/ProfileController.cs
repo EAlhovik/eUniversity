30f2b5625269c363a63f023fba15030f03523cca
EAlhovik <e.alhovik@gmail.com>
2014-01-20 13:28:05 +0300

    ajax tab for editing profile

--- Web/eUniversity.Web/Controllers/ProfileController.cs
+++ Web/eUniversity.Web/Controllers/ProfileController.cs
@@ -9,5 +9,24 @@ namespace eUniversity.Web.Controllers
             return View();
         }
 
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult BasicInfo()
+        {
+            return View(@"EditorTemplates\BasicInfo");
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View(@"EditorTemplates\ChangePassword");
+        }
+
+        public ActionResult Settings()
+        {
+            return View(@"EditorTemplates\Settings");
+        }
     }
 }
