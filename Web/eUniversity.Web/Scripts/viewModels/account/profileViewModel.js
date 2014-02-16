927a7b28600debca2471ab91c67970c981c28ace
EAlhovik <e.alhovik@gmail.com>
2014-02-16 19:22:53 +0300

    validate group on register form

--- Web/eUniversity.Web/Scripts/viewModels/account/profileViewModel.js
+++ Web/eUniversity.Web/Scripts/viewModels/account/profileViewModel.js
@@ -2,7 +2,14 @@
     var self = this;
     self.FirstName = ko.observable().extend({ required: true });
     self.LastName = ko.observable().extend({ required: true });
-    self.Group = ko.observable().extend({ required: true });
+    self.Group = ko.observable().extend({
+        validation: {
+            validator: function (val) {
+                return val != null;
+            },
+            message: 'This field is required.',
+        }
+    });
 
     ko.mapping.fromJS(serverModel, {}, self);
     
